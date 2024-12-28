248e492722b07bcf09c809e3dd206f772b94d059
Aleks Rutins <keeper@farthergate.com>
2024-12-27 20:45:27 -0500

    Implement drivebase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DriveBase.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.util;
 
 import java.lang.Math;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
 
 public class DriveBase {
   public static double wheelDiameterMM = 96.0;
@@ -26,15 +30,100 @@ public class DriveBase {
   public static double motorEncoderEventsPerInch = motorRotationsPerInch * motorEncoderEventsPerRotation;
   public static double motorEncoderEventsPerMM = motorRotationsPerMM * motorEncoderEventsPerRotation;
 
-  private DcMotorEx leftFront;
-  private DcMotorEx leftRear;
-  private DcMotorEx rightFront;
-  private DcMotorEx rightRear;
+  public static double DEFAULT_POWER = 0.2;
+
+  DcMotorEx leftFront;
+  DcMotorEx leftRear;
+  DcMotorEx rightFront;
+  DcMotorEx rightRear;
 
   public DriveBase(HardwareMap hardwareMap) {
     leftFront = (DcMotorEx) hardwareMap.dcMotor.get("left_front");
     leftRear = (DcMotorEx) hardwareMap.dcMotor.get("left_rear");
     rightFront = (DcMotorEx) hardwareMap.dcMotor.get("right_front");
     rightRear = (DcMotorEx) hardwareMap.dcMotor.get("right_rear");
+  }
+
+  void eachMotor(Consumer<DcMotorEx> cb) {
+    cb.accept(leftFront);
+    cb.accept(leftRear);
+    cb.accept(rightFront);
+    cb.accept(rightRear);
+  }
+
+  public class Movement {
+    public class Ticks {
+      public int lf;
+      public int lr;
+      public int rf;
+      public int rr;
+      public int tolerance = 1;
+
+      public Ticks(int lf, int lr, int rf, int rr) {
+        this.lf = lf;
+        this.lr = lr;
+        this.rf = rf;
+        this.rr = rr;
+      }
+
+      public Ticks relativeTo(DriveBase driveBase) {
+        Ticks result = new Ticks(driveBase.leftFront.currentPosition + lf, driveBase.leftRear.currentPosition + lr,
+            driveBase.rightFront.currentPosition + rf, driveBase.rightRear.currentPosition + rr);
+        result.tolerance = tolerance;
+      }
+
+      public void apply(DriveBase driveBase) {
+        driveBase.leftFront.setTargetPosition(lf);
+        driveBase.leftRear.setTargetPosition(lr);
+        driveBase.rightFront.setTargetPosition(rf);
+        driveBase.rightRear.setTargetPosition(rr);
+
+        driveBase.eachMotor((motor) -> motor.setTargetPositionTolerance(tolerance));
+      }
+    }
+
+    Optional<Integer> power = Optional.empty();
+    Optional<Ticks> ticks = Optional.empty();
+
+    public void run(DriveBase driveBase) {
+      if (ticks.isPresent()) {
+        driveBase.eachMotor((motor) -> motor.setMode(RunMode.RUN_USING_ENCODER));
+
+        Ticks relativeTicks = ticks.get().relativeTo(driveBase);
+        relativeTicks.apply(driveBase);
+
+        driveBase.eachMotor((motor) -> motor.setMode(RunMode.RUN_TO_POSITION));
+      }
+
+      if (power.isPresent())
+        driveBase.eachMotor((motor) -> motor.setPower(power.get()));
+    }
+
+    public class Builder {
+      Movement movement = new Movement();
+
+      public Builder() {
+      }
+
+      public Builder withPower(int power) {
+        movement.power = Optional.of(power);
+        return this;
+      }
+
+      public Builder withTicks(int lf, int lr, int rf, int rr) {
+        movement.ticks = Optional.of(new Ticks(lf, lr, rf, rr));
+        return this;
+      }
+
+      public Builder withTolerance(int tolerance) {
+        if (movement.ticks.isPresent()) {
+          movement.ticks.get().tolerance = tolerance;
+        }
+      }
+
+      public Movement build() {
+        return movement;
+      }
+    }
   }
 }
